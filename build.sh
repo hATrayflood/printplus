28d68c90a1f5384163431856835cc6e43c4863eb
hATrayflood <h.rayflood@gmail.com>
2012-07-29 13:56:27 +0900

    add command 'git add .' and rename xpi.

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 rm   -rf /tmp/printplus
 mkdir -p /tmp/printplus
+git add .
 git checkout-index -a -f --prefix=/tmp/printplus/
 cd /tmp/printplus
-rm  -f ../printplus.xpi
-zip -r ../printplus.xpi * > /dev/null
+rm  -f build.sh
+rm  -f ${OLDPWD}/../printplus-fx+tb+sm.xpi
+zip -r ${OLDPWD}/../printplus-fx+tb+sm.xpi * > /dev/null
